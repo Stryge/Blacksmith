771c751d052f5f4ed747c6028fe0e90d5d009ec3
Stryge <jaunetetienne@gmail.com>
2021-08-10 10:26:38 +0200

    utilisation d'un store pour stocker les états du player, utilisation d'un event bus pour lancer le player

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Player from './components/Player.vue'
+import { createStore } from 'vuex'
+import Mitt from 'mitt'
 
 let app = createApp(App)
+let store = createStore({
+	state: {
+		trackUrl: 'test',
+		playerState: 'pause',
+	},
+	mutations: {
+		loadTrack(state, preview_url) {
+			state.trackUrl = preview_url
+		}
+	}
+})
+let mitt = new Mitt()
+
+app.config.globalProperties.$mitt = mitt
+
+app.component("Player", Player)
+app.use(store)
 app.use(router)
 app.mount('#app')
